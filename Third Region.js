a2c048b283f66415c22b928ca6f5b388052589fd
InteresTHINGS-ygt <ygt_2004@outlook.com>
2023-11-28 23:28:38 +0300

    Size

--- Third Region.js
+++ Third Region.js
@@ -1,33 +1,29 @@
 var current_question = -1;
 var score = 0;
 
-var cerebral = 0;
-var left = 0;
-var limbic = 0;
-var right = 0;
+var emotional_freedom = 0;
+var generalism = 0;
+var causal_attitude = 0;
 
-var cerebrality = 0;
-var rightness = 0;
+var emo_f = "";
+var gen = "";
+var cas = "";
 
 var test_status = 0;
 
 function buttonclick(score) {
-    var questions = ["I am intellectually curious and have a love for learning.",
-    "I am a rational thinker who tends to contain emotions.",
-    "I am realistic and prefer concrete interests.",
-    "I am flexible and like to go with the flow.",
-    "I enjoy engaging in complex thought.",
-    "I make my decisions primarily based on facts and logic.",
-    "I am generally an instinctive and action-oriented person.",
-    "I listen to emotions and intuition while making decisions.",
-    "I feel at home with abstract concepts and ideas.",
-    "I am more of a planner than someone who acts spontaneously.",
-    "I learn best with a practical, hands-on style.",
-    "I am more people-oriented than task-oriented.",
-    "I prefer a job with high intellectual stimulation.",
-    "I highly favor efficiency and accuracy in my lifestyle.",
-    "I am better at executing ideas than generating them.",
-    "I am enthusiastic and express my feelings easily."];
+    var questions = ["I have an easy time controlling my reactions.",
+    "I tend to repress my emotions in order to stay rational.",
+    "I try to express myself as freely as possible.",
+    "I am pretty much authentic and unfiltered.",
+    "My interests are very selective and personal.",
+    "I focus on one thing to become really good at it.",
+    "I feel bored when I am not exploring new interests.",
+    "I feel the need to be good at everything.",
+    "I take my job seriously and want to perform high.",
+    "I care a lot about my performance and presentation.",
+    "I just do what I enjoy regardless of performance.",
+    "My content tends to have an \"everyday\" vibe."];
 
     if (test_status == 0) {
         test_status = 1;
@@ -38,129 +34,67 @@ function buttonclick(score) {
     current_question += 1;
     document.getElementById("question_number").innerHTML = "Question " + (current_question + 1);
     document.getElementById("Statement").innerHTML = questions[current_question];
-    if (current_question % 4 == 1){
-        cerebral = cerebral + score
+    if (current_question < 3){
+        emotional_freedom += score;
     }
-    else if (current_question % 4 == 2){
-        left = left + score
+    else if (current_question < 5){
+        emotional_freedom += (4-score);
     }
-    else if (current_question % 4 == 3){
-        limbic = limbic + score
+    else if (current_question < 7){
+        generalism += score;
     }
-    else if (current_question % 4 == 0){
-        right = right + score
+    else if (current_question < 9){
+        generalism += (4-score);
+    }
+    else if (current_question < 11){
+        causal_attitude += score;
+    }
+    else if (current_question < 13){
+        causal_attitude += (4-score);
     }
 
     if (current_question > (questions.length - 1)){
         document.getElementById("question_number").style.display = "none";
         document.getElementById("Statement").innerHTML = "Finished!";
-        cerebrality = cerebral - limbic
-        rightness = right - left
-        if (cerebrality == 0) {
-            tiebreaker_c();
+        if (emotional_freedom > 8) {
+            emo_f = "Controlled"
         }
-        else if (rightness == 0) {
-            tiebreaker_r();
+        else if (emotional_freedom < 8) {
+            emo_f = "Expressive"
         }
         else {
-            get_result();
+            emo_f = "Balanced"
         }
-    }
-    
-}
 
-function tiebreaker_c() {
-    document.getElementById("test").style.display = "none";
-    document.getElementById("tiebreaker_c").style.display = "block";
-}
+        if (generalism > 8) {
+            gen = "Specialist"
+        }
+        else if (generalism < 8) {
+            gen = "Generalist"
+        }
+        else {
+            gen = "Balanced"
+        }
 
-function tiebreaker_r() {
-    document.getElementById("test").style.display = "none";
-    document.getElementById("tiebreaker_r").style.display = "block";
-}
-
-function tiebreakerclick_c(score) {
-    cerebrality += score;
-    document.getElementById("tiebreaker_c").style.display = "none";
-    if (rightness == 0) {
-        tiebreaker_r();
-    }
-    else {
+        if (causal_attitude > 8) {
+            cas = "Serious"
+        }
+        else if (causal_attitude < 8) {
+            cas = "Casual"
+        }
+        else {
+            cas = "Balanced"
+        }
         get_result();
     }
 }
 
-function tiebreakerclick_r(score) {
-    rightness += score;
-    document.getElementById("tiebreaker_r").style.display = "none";
-    get_result();
-}
-
 function get_result() {
     document.getElementById("results").style.display = "block";
-    document.getElementById("result1").innerHTML = "Your cerebrality is " + cerebrality;
-    document.getElementById("result2").innerHTML = "Your rightness is " + rightness;
+    document.getElementById("result1").innerHTML = "Emotional Style: " + emotional_freedom + " - " + emo_f;
+    document.getElementById("result2").innerHTML = "Interest Style: " + generalism + " - " + gen;
+    document.getElementById("result3").innerHTML = "Attitude: " + causal_attitude + " - " + cas;
     document.getElementById("test").style.display = "none";
     document.getElementById("question_number").style.display = "none";
-
-    if (cerebrality > 8){
-        if (rightness > 8){
-            document.getElementById("result3").innerHTML = "You are a fascinator!";
-        }
-        else if (rightness <= 8 && rightness > 0){
-            document.getElementById("result3").innerHTML = "You are an inventor!";
-        }
-        else if (rightness >= -8 && rightness < 0){
-            document.getElementById("result3").innerHTML = "You are a scientist!";
-        }
-        else {
-            document.getElementById("result3").innerHTML = "You are a technician!";
-        }
-    }
-    else if (cerebrality <= 8 && cerebrality > 0){
-        if (rightness > 8){
-            document.getElementById("result3").innerHTML = "You are an adventurer!";
-        }
-        else if (rightness <= 8 && rightness > 0){
-            document.getElementById("result3").innerHTML = "You are an entrepreneur!";
-        }
-        else if (rightness >= -8 && rightness < 0){
-            document.getElementById("result3").innerHTML = "You are a professor!";
-        }
-        else {
-            document.getElementById("result3").innerHTML = "You are an architect!";
-        }
-    }
-    else if (cerebrality >= -8 && cerebrality < 0){
-        if (rightness > 8){
-            document.getElementById("result3").innerHTML = "You are an influencer!";
-        }
-        else if (rightness <= 8 && rightness > 0){
-            document.getElementById("result3").innerHTML = "You are a coach!";
-        }
-        else if (rightness >= -8 && rightness < 0){
-            document.getElementById("result3").innerHTML = "You are a go-getter!";
-        }
-        else {
-            document.getElementById("result3").innerHTML = "You are an inspector!";
-        }
-    }
-    else {
-        if (rightness > 8){
-            document.getElementById("result3").innerHTML = "You are an entertainer!";
-        }
-        else if (rightness <= 8 && rightness > 0){
-            document.getElementById("result3").innerHTML = "You are a caregiver!";
-        }
-        else if (rightness >= -8 && rightness < 0){
-            document.getElementById("result3").innerHTML = "You are a guardian!";
-        }
-        else {
-            document.getElementById("result3").innerHTML = "You are an administrator!";
-        }
-    }
 }
-
-
-
 
